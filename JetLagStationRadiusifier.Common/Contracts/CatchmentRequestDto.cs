e5c5908748e5b524d024e89acd98d6d6e97ad318
Freddie Mason <fmason@redev.co.uk>
2026-09-20 14:57:57 +0100

    added summaries

--- JetLagStationRadiusifier.Common/Contracts/CatchmentRequestDto.cs
+++ JetLagStationRadiusifier.Common/Contracts/CatchmentRequestDto.cs
@@ -10,32 +10,28 @@ public sealed record CatchmentRequestDto
     public required Stream InputKmlStream { get; init; }
 
     /// <summary>
-    /// 
+    /// The value of the circumferance radius selected by the user.
+    /// See RadiusUnit for the units of the radius.
     /// </summary>
     public required int Radius { get; init; }
 
     /// <summary>
-    /// 
+    /// The unit that Radius is in
     /// </summary>
     public required DistanceUnit RadiusUnit { get; init; }
 
     /// <summary>
-    /// 
+    /// The amount of red for the colour of the drawn radius.
     /// </summary>
     public required byte Red { get; init; }
 
     /// <summary>
-    /// 
+    /// The amount of green for the colour of the drawn radius.
     /// </summary>
     public required byte Green { get; init; }
 
     /// <summary>
-    /// 
+    /// The amount of blue for the colour of the drawn radius.
     /// </summary>
     public required byte Blue { get; init; }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    public string LayerName { get; init; } = "Stations";
 }
