7809492b78c356a6f2471ec7d360af5c3e3cfe32
Freddie Mason <fmason@redev.co.uk>
2026-09-20 14:58:13 +0100

    added ColourHelper

---
+++ JetLagStationRadiusifier.Common/Helpers/ColourHelper.cs
@@ -0,0 +1,27 @@
+﻿using System.Drawing;
+
+namespace JetLagStationRadiusifier.Common.Helpers;
+
+public static class ColourHelper
+{
+    public static byte GetRedBytesFromHex(string hex)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(hex);
+        var colour = ColorTranslator.FromHtml(hex);
+        return colour.R;
+    }
+
+    public static byte GetGreenBytesFromHex(string hex)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(hex);
+        var colour = ColorTranslator.FromHtml(hex);
+        return colour.G;
+    }
+
+    public static byte GetBlueBytesFromHex(string hex)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(hex);
+        var colour = ColorTranslator.FromHtml(hex);
+        return colour.B;
+    }
+}
